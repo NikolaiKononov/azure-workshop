c00052ac4287f2a80e362bdcf6985c1b47b5aae1
NikolaiKononov <nikolai.kononov@cytiva.com>
2023-12-13 16:25:39 +0100

    add azur func handler

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,13 +2,23 @@ import './app.css';
 
 function main(): void {
     const button = document.querySelector("#testButton");
+    const azureBtn = document.querySelector("#azureBtn");
     button?.addEventListener("click", testSubmitHandler);
+    azureBtn?.addEventListener("click", testAzureFuncHandler);
 }
 
 function testSubmitHandler(e: Event): void {
     e.preventDefault();
     const outputElement = document.querySelector('#output');
     outputElement.textContent = 'Scripts work!';
+}
+
+async function testAzureFuncHandler(e: Event): Promise<void> {
+    e.preventDefault();
+    const apiResponse = await fetch('/api/myHttpTrigger')
+    const apiResponseText = await apiResponse.text()
+    const outputElement = document.querySelector('#azureParagraphOutput');
+    outputElement.textContent = apiResponseText;
 }
 
 main();
